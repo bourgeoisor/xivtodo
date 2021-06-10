a3db46f14943a0bd2378f6c069b357d74b588dfe
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2021-06-10 13:42:39 -0300

    Add vue-gtag configuration

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag";
 
 store.commit("initialiseStore");
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(
+    VueGtag,
+    {
+      config: {
+        id: "G-9WXY5M43Z0",
+      },
+    },
+    router
+  )
+  .mount("#app");
 
 store.subscribe((_mutation, state) => {
   localStorage.setItem("store", JSON.stringify(state));
