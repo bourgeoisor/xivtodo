75109b30d3b8fe8a0b25018629adb02a192978df
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2021-06-15 19:26:37 -0300

    Add About and FAQ pages

---
+++ src/views/FAQ.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>FAQ</h1>
+    <hr />
+    <div class="row">
+      <div class="col-lg-10">
+        <h2>Question</h2>
+        <p>Answer</p>
+        <h2>Question</h2>
+        <p>Answer</p>
+        <h2>Question</h2>
+        <p>Answer</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FAQ",
+};
+</script>
