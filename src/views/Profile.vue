4ac32a18b6bcd26f3c462e83e5bc6810ce0273e6
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2021-10-27 23:42:31 -0400

    Disable the Elemental and Bozjan levels from Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -167,6 +167,7 @@
           </div>
         </div>
         <br />
+        <!-- @TODO: https://github.com/bourgeoisor/xivtodo/issues/14
         <div class="info-block">
           <b>{{ $t("profile.elementalLevel") }}</b><br />
           <span
@@ -189,7 +190,7 @@
             {{ this.$store.getters.character.ClassJobBozjan?.Level || "0" }}
           </span>
         </div>
-        <br />
+        <br /> -->
       </div>
     </div>
   </div>
