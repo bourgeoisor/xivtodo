9c6d814687beeab2fbab7676d342fe699567bb2b
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2022-12-05 20:15:14 -0500

    Add "character updating" feedback to Checklist page

--- src/views/Checklist.vue
+++ src/views/Checklist.vue
@@ -5,6 +5,10 @@
         {{ $t("page.checklist") }}
         <span v-if="this.$store.getters.hasCharacter" class="fs-3 fw-lighter">
           {{ $t("message.forCharacter", { characterName: this.$store.getters.character.Name }) }}
+          <div v-if="this.$store.getters.activeCharacterOutOfDate" class="text-info fs-6">
+            <div class="spinner-border spinner-border-sm" role="status"></div>
+            Updating character data, this may take a minute...
+          </div>
         </span>
       </span>
       <button
